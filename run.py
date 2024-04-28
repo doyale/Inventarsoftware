12e5063a555cf958e777c0c6b5975145f014db94
doyale <ldoyale.lp@gmail.com>
2024-04-28 22:10:34 +0200

    added run.py to launch scripts concurrently

---
+++ run.py
@@ -0,0 +1,15 @@
+import threading
+import subprocess
+
+def run_script(script_name):
+    subprocess.run(["python", script_name])
+
+if __name__ == "__main__":
+    script1_thread = threading.Thread(target=run_script, args=("UI_main.py",))
+    script2_thread = threading.Thread(target=run_script, args=("eln_window.py",))
+
+    script1_thread.start()
+    script2_thread.start()
+
+    script1_thread.join()
+    script2_thread.join()
